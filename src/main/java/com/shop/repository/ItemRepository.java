14a1809053add5f655b3bcfd4dbedcf42b56c812
vinszip999 <w2011@e-mirim.hs.kr>
2022-05-20 09:31:28 +0900

    220520 수업(LessThan 조건 처리하기, Order By로 정렬 처리하기)

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -9,4 +9,8 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByItemNm(String itemNm); // 추상메서드
 
     List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price); // Desc : 내림차순
 }
